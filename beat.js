58eec823263923674c7992747fdd19d59407c8f8
donovan-k <48697778+donovan-k@users.noreply.github.com>
2023-09-20 13:27:09 +0000

    added ability to play song after one ends

--- beat.js
+++ beat.js
@@ -1,58 +1,67 @@
-        window.addEventListener('DOMContentLoaded', (event) => {
-            const audioElements = document.querySelectorAll('audio');
-            const playButtons = document.querySelectorAll('.play-btn');
-            const stopButton = document.getElementById('stop-btn');
-            const shuffleButton = document.getElementById('shuffle-btn');
-            const nextButton = document.getElementById('next-btn');
-            const prevButton = document.getElementById('prev-btn');
+window.addEventListener('DOMContentLoaded', (event) => {
+    const audioElements = document.querySelectorAll('audio');
+    const playButtons = document.querySelectorAll('.play-btn');
+    const stopButton = document.getElementById('stop-btn');
+    const shuffleButton = document.getElementById('shuffle-btn');
+    const nextButton = document.getElementById('next-btn');
+    const prevButton = document.getElementById('prev-btn');
 
-            // Initialize the player with the first audio element
-            let currentPlayer = audioElements[0];
+    // Initialize the player with the first audio element
+    let currentPlayer = audioElements[0];
 
-            // Play the selected audio element
-            function playAudio(audioElement) {
-                audioElements.forEach((element) => {
-                    if (element !== audioElement) {
-                        element.pause();
-                    }
-                });
-                audioElement.play();
-                currentPlayer = audioElement;
+    // Play the selected audio element
+    function playAudio(audioElement) {
+        audioElements.forEach((element) => {
+            if (element !== audioElement) {
+                element.pause();
             }
-
-            // Add event listeners to play/pause buttons
-            playButtons.forEach((button) => {
-                button.addEventListener('click', () => {
-                    const audioElement = button.parentNode.querySelector('audio');
-                    playAudio(audioElement);
-                });
-            });
-
-            // Add event listener to stop button
-            stopButton.addEventListener('click', () => {
-                audioElements.forEach((element) => {
-                    element.pause();
-                    element.currentTime = 0;
-                });
-            });
-
-            // Add event listener to shuffle button
-            shuffleButton.addEventListener('click', () => {
-                const shuffledAudioElements = Array.from(audioElements).sort(() => Math.random() - 0.5);
-                playAudio(shuffledAudioElements[0]);
-            });
-
-            // Add event listener to next button
-            nextButton.addEventListener('click', () => {
-                const currentIndex = Array.from(audioElements).indexOf(currentPlayer);
-                const nextIndex = (currentIndex + 1) % audioElements.length;
-                playAudio(audioElements[nextIndex]);
-            });
-
-            // Add event listener to previous button
-            prevButton.addEventListener('click', () => {
-                const currentIndex = Array.from(audioElements).indexOf(currentPlayer);
-                const prevIndex = (currentIndex - 1 + audioElements.length) % audioElements.length;
-                playAudio(audioElements[prevIndex]);
-            });
         });
+        audioElement.play();
+        currentPlayer = audioElement;
+    }
+
+    // Add event listeners to play/pause buttons
+    playButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const audioElement = button.parentNode.querySelector('audio');
+            playAudio(audioElement);
+        });
+    });
+
+    // Add event listener to stop button
+    stopButton.addEventListener('click', () => {
+        audioElements.forEach((element) => {
+            element.pause();
+            element.currentTime = 0;
+        });
+    });
+
+    // Add event listener to shuffle button
+    shuffleButton.addEventListener('click', () => {
+        const shuffledAudioElements = Array.from(audioElements).sort(() => Math.random() - 0.5);
+        playAudio(shuffledAudioElements[0]);
+    });
+
+    // Add event listener to next button
+    nextButton.addEventListener('click', () => {
+        const currentIndex = Array.from(audioElements).indexOf(currentPlayer);
+        const nextIndex = (currentIndex + 1) % audioElements.length;
+        playAudio(audioElements[nextIndex]);
+    });
+
+    // Add event listener to previous button
+    prevButton.addEventListener('click', () => {
+        const currentIndex = Array.from(audioElements).indexOf(currentPlayer);
+        const prevIndex = (currentIndex - 1 + audioElements.length) % audioElements.length;
+        playAudio(audioElements[prevIndex]);
+    });
+
+    // Add event listener to automatically play the next shuffled song when the current one ends
+    audioElements.forEach((audioElement) => {
+        audioElement.addEventListener('ended', () => {
+            const currentIndex = Array.from(audioElements).indexOf(audioElement);
+            const nextIndex = (currentIndex + 1) % audioElements.length;
+            playAudio(audioElements[nextIndex]);
+        });
+    });
+});
